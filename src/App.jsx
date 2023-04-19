a2d2a903e6e43ffeeeb0a638bbada0cb70713245
leonellcruz7 <leonellcruz111513@gmail.com>
2023-04-20 01:37:30 +0800

    fix routes bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./App.scss";
 import "remixicon/fonts/remixicon.css";
-import { BrowserRouter } from "react-router-dom";
-import { Route, Routes } from "../node_modules/react-router-dom/dist/index";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/authentication/Login";
 import Signup from "./pages/authentication/Signup";
 import Feed from "./pages/Feed";
