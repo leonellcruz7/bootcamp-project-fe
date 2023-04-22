8576d3636b1f690ad72c3389474e618c3a8f1ee5
leonellcruz7 <leonellcruz111513@gmail.com>
2023-04-23 00:46:08 +0800

    update scroll bar style

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -7,10 +7,14 @@ const PostContent: FC<PostContentType> = ({ content }) => {
     <a href={`/view-post/${content?.id}`} className="button link">
       <div className="post-content mt-2">
         <p className="font-bold text-md truncate max-w-[400px]">
-          {content?.title}{" "}
+          {content?.title}
         </p>
         <div>
-          <p className="font-light text-sm mb-1 break-words">{content?.body}</p>
+          <div className="overflow-y-scroll max-h-[190px] my-1 pr-5">
+            <p className="font-light text-sm mb-1 break-words">
+              {content?.body}
+            </p>
+          </div>
           <div className="image-holder">
             {/* <img
               src="https://res.cloudinary.com/dyecs1c3j/image/upload/v1681873352/Frame_13_pya8f9.png"
