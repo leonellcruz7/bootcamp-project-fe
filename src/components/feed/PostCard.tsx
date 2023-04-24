07f5f6c83e7be1eee1cb3bfd967ea207ddac782a
leonellcruz7 <leonellcruz111513@gmail.com>
2023-04-24 10:01:53 +0800

    add loading cards while fetching data

--- src/components/feed/PostCard.tsx
+++ src/components/feed/PostCard.tsx
@@ -14,6 +14,7 @@ import {
 } from "../../actions/posts";
 import Cookies from "universal-cookie";
 import { deleteButtonVariants } from "../../framer-motion/variants";
+import PostCardLoading from "../LoadingCards/PostCardLoading";
 
 const PostCard: FC<PostTypes> = ({ post, update, setUpdate }) => {
   const [info, setInfo] = useState<any>();
@@ -68,81 +69,89 @@ const PostCard: FC<PostTypes> = ({ post, update, setUpdate }) => {
 
   const navigate = useNavigate();
   return (
-    <div className="postcard-container">
-      <div className="actions-container">
-        <div className="vote-wrapper">
-          <button
-            onClick={handleUpvote}
-            disabled={votes?.includes(current_user)}
-          >
-            <i
-              className={`icon ri-arrow-up-line ${
-                votes?.includes(current_user) && "active"
-              }`}
-            ></i>
-          </button>
-          <p className="text-neutral800">{votes?.length}</p>
-          <button
-            onClick={handleDownvote}
-            disabled={!votes?.includes(current_user)}
-          >
-            <i className="icon ri-arrow-down-line"></i>
-          </button>
-        </div>
-        <div className="control-wrapper">
-          <button
-            onClick={() => handleNavigate(navigate, `/view-post/${post.id}`)}
-          >
-            <i className="icon ri-chat-3-line"></i>
-          </button>
-          <button onClick={handleShare}>
-            <i className="icon ri-share-forward-line"></i>
-          </button>
-          {post_owner === current_user && (
-            <>
+    <>
+      {!loading ? (
+        <div className="postcard-container">
+          <div className="actions-container">
+            <div className="vote-wrapper">
+              <button
+                onClick={handleUpvote}
+                disabled={votes?.includes(current_user)}
+              >
+                <i
+                  className={`icon ri-arrow-up-line ${
+                    votes?.includes(current_user) && "active"
+                  }`}
+                ></i>
+              </button>
+              <p className="text-neutral800">{votes?.length}</p>
+              <button
+                onClick={handleDownvote}
+                disabled={!votes?.includes(current_user)}
+              >
+                <i className="icon ri-arrow-down-line"></i>
+              </button>
+            </div>
+            <div className="control-wrapper">
               <button
                 onClick={() =>
-                  handleNavigate(navigate, `/edit-post/${post.id}`)
+                  handleNavigate(navigate, `/view-post/${post.id}`)
                 }
               >
-                <i className="icon ri-pencil-fill"></i>
+                <i className="icon ri-chat-3-line"></i>
               </button>
-              <div className="relative">
-                <button onClick={handleDeleteAnimate}>
-                  <i className="icon ri-delete-bin-6-line"></i>
-                </button>
-                <motion.div
-                  className="absolute top-0 right-[-240px] flex gap-1"
-                  variants={deleteButtonVariants}
-                  animate={buttonAnimate}
-                  initial="close"
-                >
-                  <div className="flex items-center gap-2">
-                    <p className="truncate min-w-[100px]">Are you sure?</p>
-                    <button
-                      onClick={handleDeleteAnimate}
-                      className="button primary-button small"
-                    >
-                      no
+              <button onClick={handleShare}>
+                <i className="icon ri-share-forward-line"></i>
+              </button>
+              {post_owner === current_user && (
+                <>
+                  <button
+                    onClick={() =>
+                      handleNavigate(navigate, `/edit-post/${post.id}`)
+                    }
+                  >
+                    <i className="icon ri-pencil-fill"></i>
+                  </button>
+                  <div className="relative">
+                    <button onClick={handleDeleteAnimate}>
+                      <i className="icon ri-delete-bin-6-line"></i>
                     </button>
-                    <button
-                      onClick={handleDelete}
-                      className="button primary-button danger small"
+                    <motion.div
+                      className="absolute top-0 right-[-240px] flex gap-1"
+                      variants={deleteButtonVariants}
+                      animate={buttonAnimate}
+                      initial="close"
                     >
-                      yes
-                    </button>
+                      <div className="flex items-center gap-2">
+                        <p className="truncate min-w-[100px]">Are you sure?</p>
+                        <button
+                          onClick={handleDeleteAnimate}
+                          className="button primary-button small"
+                        >
+                          no
+                        </button>
+                        <button
+                          onClick={handleDelete}
+                          className="button primary-button danger small"
+                        >
+                          yes
+                        </button>
+                      </div>
+                    </motion.div>
                   </div>
-                </motion.div>
-              </div>
-            </>
-          )}
+                </>
+              )}
+            </div>
+          </div>
+          <div className="content-wrapper">
+            <UserInformation info={info} />
+            <PostContent content={post.attributes} />
+          </div>
         </div>
-      </div>
-      <div className="content-wrapper">
-        <UserInformation info={info} />
-        <PostContent content={post.attributes} />
-      </div>
-    </div>
+      ) : (
+        <PostCardLoading />
+      )}
+    </>
   );
 };
 
